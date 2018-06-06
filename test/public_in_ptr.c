00ae7cce6444f8bfda935c0aded2d3413b9f7c26
Shaobo He <polarishehn@gmail.com>
2018-06-06 14:48:04 -0400

    Updated the test case

--- test/public_in_ptr.c
+++ test/public_in_ptr.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "ct-fuzz.h"
 
 CT_FUZZ_SPEC(foo)(char* a) {
+  __ct_fuzz_public_in(a, 2);
 }
 
 int foo(char* a) {
+  printf("%lu\n", (unsigned long)malloc(1));
+  printf("We're in: %d\n", getpid());
   printf("%c..%c\n", a[0], a[1]);
   return *a + 1;
 }
