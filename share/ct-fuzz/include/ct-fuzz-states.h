e475d2de8d66f1f80cafad801b3b82e9a3086046
Shaobo He <polarishehn@gmail.com>
2018-06-07 11:46:43 -0400

    Rewrote ct_fuzz.c

---
+++ share/ct-fuzz/include/ct-fuzz-states.h
@@ -0,0 +1,27 @@
+#ifndef CT_FUZZ_STATES
+#define CT_FUZZ_STATES
+
+#include <sys/mman.h>
+#include "ct-fuzz-utils.h"
+
+// unhealthy but necessary?
+#define HASH_T TYPE(hash)
+#define IDX_T TYPE(idx)
+#define MONITORS PREFIX(monitors)
+#define RUN_ID PREFIX(run_idx)
+
+typedef unsigned long long HASH_T; 
+typedef unsigned char IDX_T;
+
+HASH_T* PREFIX(monitors);
+IDX_T RUN_ID;
+
+void* PREFIX(create_shared_memory)(size_t size) {
+  return mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+}
+
+void* PREFIX(initialize_states)() {
+  MONITORS = (HASH_T*)PREFIX(create_shared_memory)(2*sizeof(HASH_T));
+}
+
+#endif
--- share/ct-fuzz/lib/ct_fuzz.c
+++ share/ct-fuzz/lib/ct_fuzz.c
@@ -2,15 +2,16 @@
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/mman.h>
 #include <stdio.h>
 #include <sys/wait.h>
+//#include "ct-fuzz-debug.h"
+#include "ct-fuzz-states.h"
 
 #define PUBLIC_VALUE_MAX_COUNT 1000
 
-typedef unsigned char idx_t;
+#define ASSUME PREFIX(assume)
 
-void __ct_fuzz_assume(bool cond, char* msg) {
+void ASSUME(bool cond, char* msg) {
   if (!cond) {
     if (msg)
       printf("%s\n", msg);
@@ -18,96 +19,64 @@ void __ct_fuzz_assume(bool cond, char* msg) {
   }
 }
 
-size_t __ct_fuzz_size_t_max(size_t a, size_t b){
+size_t PREFIX(size_t_max)(size_t a, size_t b){
   if (a > b)
     return a;
   else
     return b;
 }
 
-idx_t __ct_fuzz_run_idx;
-
-void __ct_fuzz_memcpy_wrapper(char* dest, char* src, size_t num) {
+void PREFIX(memcpy_wrapper)(char* dest, char* src, size_t num) {
   memcpy(dest, src, num);
 }
 
-void __ct_fuzz_handle_public_value(char* src, size_t size) {
-  static char* __ct_fuzz_public_values[PUBLIC_VALUE_MAX_COUNT] = {NULL};
-  static unsigned __ct_fuzz_public_value_update_idx = 0;
-  static unsigned __ct_fuzz_public_value_check_idx = 0;
+void PREFIX(handle_public_value)(char* src, size_t size) {
+  static char* PREFIX(public_values[PUBLIC_VALUE_MAX_COUNT]) = {NULL};
+  static unsigned PREFIX(public_value_update_idx) = 0;
+  static unsigned PREFIX(public_value_check_idx) = 0;
 
-  if (!__ct_fuzz_run_idx) {
+  if (!RUN_ID) {
     char* dest = (char*)malloc(size);
-    __ct_fuzz_public_values[__ct_fuzz_public_value_update_idx++] = dest;
-    __ct_fuzz_memcpy_wrapper(dest, src, size);
+    PREFIX(public_values)[PREFIX(public_value_update_idx)++] = dest;
+    PREFIX(memcpy_wrapper)(dest, src, size);
   } else {
-    char* v = __ct_fuzz_public_values[__ct_fuzz_public_value_check_idx++];
-    __ct_fuzz_assume(memcmp(src, v, size) == 0, "Public values mismatch.");
+    char* v = PREFIX(public_values)[PREFIX(public_value_check_idx)++];
+    ASSUME(memcmp(src, v, size) == 0, "Public values mismatch.");
   }
 }
 
-void __ct_fuzz_stdin_read(void* buf, size_t size) {
+void PREFIX(stdin_read)(void* buf, size_t size) {
   read(0, buf, size);
 }
 
-void* create_shared_memory(size_t size) {
-  return mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
-}
 
-void __ct_fuzz_initialize(void) {
-  // nothing to do
+void PREFIX(initialize)(void) {
+  PREFIX(initialize_states);
 }
 
 //void __ct_fuzz_switch(void) {
 //  __ct_fuzz_run_idx += 1;
 //}
 
-void __ct_fuzz_read_inputs(void);
-/* supposed body
- * {
- *  read pub_input_i
- *  read private_input_i
- *  ...
- *  read pub_input_i'
- *  read private_input_i'
- *  check pub_input_i, pub_input_i' (call spec)
- *  ...
- *
- *  write public_input_i into pipe
- *  write private_input_i into pipe
- *  ...
- *  write public_input_i' into pipe
- *  write private_input_i' into pipe
- *
- * }
- */
+void PREFIX(exec)(IDX_T);
 
-void __ct_fuzz_exec(idx_t);
-/* supposed body
- * read input from pipe
- * call function in test
- */
+void PREFIX(spec)(IDX_T);
 
-void __ct_fuzz_spec(idx_t);
+void PREFIX(main)(void) {
+  PREFIX(initialize)();
+  PREFIX(read_inputs)();
 
-void __ct_fuzz_check_observations() {
-  // TODO
-}
+  // make this loop a macro? Yikes.
+  for (RUN_ID = 0; RUN_ID < 2; ++RUN_ID)
+    PREFIX(spec)(RUN_ID);
 
-void __ct_fuzz_main(void) {
-  __ct_fuzz_initialize();
-  __ct_fuzz_read_inputs();
-
-  for (__ct_fuzz_run_idx = 0; __ct_fuzz_run_idx < 2; ++__ct_fuzz_run_idx)
-    __ct_fuzz_spec(__ct_fuzz_run_idx);
-
-  for (__ct_fuzz_run_idx = 0; __ct_fuzz_run_idx < 2; ++__ct_fuzz_run_idx) {
+  for (RUN_ID = 0; RUN_ID < 2; ++RUN_ID) {
     pid_t pid = fork();
     if (pid == -1)
       exit(EXIT_FAILURE);
     else if (pid == 0) {
       // in the child process, good luck everybody else
-      __ct_fuzz_exec(__ct_fuzz_run_idx);
+      PREFIX(exec)(RUN_ID);
       exit(EXIT_SUCCESS);
     }
     else {
@@ -116,10 +85,10 @@ void __ct_fuzz_main(void) {
     }
   }
 
-  __ct_fuzz_check_observations();
+  //__ct_fuzz_check_observations();
 }
 
 int main(void) {
-  __ct_fuzz_main();
+  PREFIX(main)();
   return 0;
 }
