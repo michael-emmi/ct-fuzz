d46183c4f51f4b3a513e1a3e0f2d430b03fd0a47
Shaobo He <polarishehn@gmail.com>
2018-06-21 11:19:15 -0700

    Added book-keeping information to pointers

--- share/ct-fuzz/include/ct-fuzz-read.h
+++ share/ct-fuzz/include/ct-fuzz-read.h
@@ -3,9 +3,15 @@
 #include <stdint.h>
 #include "ct-fuzz-utils.h"
 
+#define ARR_INFO_SIZE 100
+#define ARR_INFO_T PREFIX(arr_info)
+#define ARR_INFO_COUNTER PREFIX(arr_info_counter)
+#define ARR_INFO_LIST PREFIX(array_info_list)
+
 typedef uint16_t len_t;
 typedef void (*func_t)(char*);
 
-extern void PREFIX(stdin_read)(void* buf, size_t size);
 extern len_t PREFIX(read_array_len)(void);
+extern len_t PREFIX(get_arr_len)(char*);
+extern void PREFIX(stdin_read)(void* buf, size_t size);
 extern void PREFIX(read_ptr_generic)(char**, size_t, unsigned, func_t);
--- share/ct-fuzz/lib/ct-fuzz-read.c
+++ share/ct-fuzz/lib/ct-fuzz-read.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 #include "ct-fuzz-read.h"
+
+typedef struct {
+  char* ptr;
+  len_t len;
+} ARR_INFO_T;
+
+static ARR_INFO_T* ARR_INFO_LIST[ARR_INFO_SIZE];
+static unsigned char ARR_INFO_COUNTER = 0;
+
+static void PREFIX(set_arr_len)(char* ptr, len_t len) {
+  ARR_INFO_T* ai = (ARR_INFO_T*)malloc(sizeof(ARR_INFO_T));
+  ai->ptr = ptr;
+  ai->len = len;
+  ARR_INFO_LIST[ARR_INFO_COUNTER++] = ai;
+}
+
+len_t PREFIX(get_arr_len)(char* ptr) {
+  for (unsigned char i = 0; i < ARR_INFO_COUNTER; ++i)
+    if (ARR_INFO_LIST[i]->ptr == ptr)
+      return ARR_INFO_LIST[i]->len;
+  assert(0 && "not a valid pointer to query array length");
+}
 
 void PREFIX(stdin_read)(void* buf, size_t size) {
   // not sure if the if condition is required.
@@ -18,10 +41,11 @@ len_t PREFIX(read_array_len)(void) {
 void PREFIX(read_ptr_generic)(char** ppt, size_t es, unsigned indirection, func_t callback) {
   len_t len = PREFIX(read_array_len)();
   size_t rs = (indirection? sizeof(char*) : es);
-  printf("len: %u\n", len);
+  //printf("len: %u\n", len);
   *ppt = malloc(rs*len);
-  printf("indirection: %u\n", indirection);
-  printf("element size: %u\n", es);
+  PREFIX(set_arr_len)(*ppt, len);
+  //printf("indirection: %u\n", indirection);
+  //printf("element size: %u\n", es);
   for (len_t i = 0; i < len; ++i)
     if (indirection)
       PREFIX(read_ptr_generic)(*ppt + rs*i, es, --indirection, callback);
